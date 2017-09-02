5d08f9610545c33716d4f37d0fa490afa852abf0
Erik P. Hansen <eph@erikphansen.com>
2017-09-02 13:12:47 -0700

    Minor regex improvement.

--- Open URLs.lbaction/Contents/Scripts/default.js
+++ Open URLs.lbaction/Contents/Scripts/default.js
@@ -3,7 +3,7 @@ function run(argument) {
     LaunchBar.alert('No argument was passed to the action');
   } else {
     // Presenting the laziest URL matcher eva
-    var urls = argument.toLowerCase().match(/https?:\S+/g);
+    var urls = argument.match(/https?:\S+/gi);
     if (urls.length == 0) {
       LaunchBar.alert('No URLs found');
     }
